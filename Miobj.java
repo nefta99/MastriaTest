dfdc321cc6c8c72c6ea05c7f53c92d35d36ee38a
nef <neftalitorres@itesm.mx>
2020-09-05 12:30:13 -0500

    desarrollo: cambios se agregan mas variables

--- Miobj.java
+++ Miobj.java
@@ -1,8 +1,21 @@
+import  java.io.*;
 public class Miobj{
    
-    //Metodo inicial del programa
+    
+    
     public static void main(String[] args)
     {
-        System.out.println("Hola Mundo");
+        /*Lo arreglos se almacen con logica es decir, cero ese el primer elemento
+        System.out.println("String[0] = "+args[0] +" ; String[1] = " + args[1]);
+        */
+        //obtener el objeto desde consola
+        Console cmd = System.console();
+        //Lee la lina de la consola y almaceno en variable nombre
+        String textoNonbre = cmd.readLine("ingresa Nombre: ");
+        //Lee la lina de la consola y almaceno en variable nombre
+        String textoApellido = cmd.readLine("ingresa Apellido: ");
+        System.out.println("El nombre es  "+textoNonbre +" el Apellido "+textoApellido);
+        
+        
     }
 }
